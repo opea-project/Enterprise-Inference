ec10e57746b8e848a3512b38a930a0cce5e7f496
gopal-raj-suresh <gopal.raj.dummugudupu@cloud2labs.com>
2026-02-04 20:54:12 -0800

    fixed api endpoint url and update .env.example
    Signed-off-by: gopal-raj-suresh <gopal.raj.dummugudupu@cloud2labs.com>

--- sample_solutions/PDFToPodcast/ui/src/hooks/index.js
+++
@@ -1,4 +0,0 @@
-export { usePolling } from './usePolling';
-export { useAudioPlayer } from './useAudioPlayer';
-export { useWaveSurfer } from './useWaveSurfer';
-export { useDebounce } from './useDebounce';
